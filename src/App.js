addaad5230af27f1f8ef2d9d87cf64337e544107
Stanley <snicholl@gmail.com>
2017-12-22 10:49:50 -0800

    added component files

--- src/App.js
+++ src/App.js
@@ -1,18 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login'
+import SignIn from './components/SignIn'
+import GettingStarted from './components/GettingStarted'
+import Guided1 from './components/Guided1'
+import Guided2 from './components/Guided2'
+import Guided3 from './components/Guided3'
+import Guided4 from './components/Guided4'
+import Guided5 from './components/Guided5'
+import SelectedSchedule from './components/SelectedSchedule'
+import UpcomingWeek from './components/UpcomingWeek'
+import MyProfile from './components/MyProfile'
+import ScheduleDashboard from './components/ScheduleDashboard'
+import InProgressSchedule from './components/InProgressSchedule'
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      page: 'login'
+    }
+  }
+
+  pages = {
+    login: <Login />,
+    signIn: <SignIn />
+    gettingStarted: <GettingStarted />,
+    guided1: <Guided1 />,
+    guided2: <Guided2 />,
+    guided3: <Guided3 />,
+    guided4: <Guided4 />,
+    guided5: <Guided5 />,
+    selectedSchedule: <SelectedSchedule />,
+    upcomingWeek: <UpcomingWeek />,
+    myProfile: <MyProfile />,
+    scheduleDashboard: <ScheduleDashboard />,
+    inProgressSchedule: <InProgressSchedule/>
+  }
+
+  updatePageState = (page) => {
+    this.setState({page: page})
+  }
+
+  showPage = (page) => {
+     return (this.pages[page])
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.showPage(this.state.page)}
       </div>
     );
   }
