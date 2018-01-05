a2b2714af8adfc87b98b3b7a49adf83e6901336c
Kevin Springer <KevinUSCU@outlook.com>
2018-01-04 22:42:56 -0800

    Wire up components with props.

--- src/components/inProgress/ItemsContainer.js
+++ src/components/inProgress/ItemsContainer.js
@@ -1,22 +1,19 @@
 import React from 'react'
 import Item from './Item'
 
-const ItemsContainer = () => {
+function mapItems(current, items) {
+  return items.map((el, index) => {
+    let itemStatus = 'upcoming'
+    if (el === current) itemStatus = 'current'
+    if (el < current) itemStatus = 'previous'
+    return <Item key={ index } itemStatus={ itemStatus } title={ el.name } timeMinutes={ el.timeMinutes } />
+  })
+}
+
+const ItemsContainer = ({ currentItemIndex, items }) => {
   return (
     <div className="container" style={{ overflow: 'scroll', height: '100vh', paddingTop: '165px', paddingBottom: '70px' }}>
-      <Item style='previous' title='yawn' time='10'/>
-      <Item style='previous' title='get dressed' time='10'/>
-      <Item style='current' title='shower' time='15'/>
-      <Item style='upcoming' title='eat breakfast' time='12'/>
-      <Item style='upcoming' title='yodel' time='15'/>
-      <Item style='upcoming' title='yodel' time='15'/>
-      <Item style='upcoming' title='yodel' time='15'/>
-      <Item style='upcoming' title='yodel' time='15'/>
-      <Item style='upcoming' title='yodel' time='15'/>
-      <Item style='upcoming' title='yodel' time='15'/>
-      <Item style='upcoming' title='yodel' time='15'/>
-      <Item style='upcoming' title='yodel' time='15'/>
-      <Item style='upcoming' title='yodel' time='15'/>
+      { items ? mapItems(currentItemIndex, items) : 'There are no events for this plan' }
     </div>
   )
 }
