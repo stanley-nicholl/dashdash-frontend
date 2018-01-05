e6cd8b7d14febeec86f8782c5e694879d764df50
Kevin Springer <KevinUSCU@outlook.com>
2018-01-05 02:40:12 -0800

    Add missing img alt.

--- src/components/common-elements/Navigation.js
+++ src/components/common-elements/Navigation.js
@@ -6,7 +6,7 @@ const Navigation = ({closeNav}) => {
   return (
     <div className="navigation sidenav" id="side-nav">
       <div className="nav-top d-flex justify-content-end">
-        <div className="d-flex justify-content-end"><img src="./img/branding/x-darkblue.svg" className="close-img mr-2 mt-3" onClick={e => closeNav()}/></div>
+        <div className="d-flex justify-content-end"><img src="./img/branding/x-darkblue.svg" alt="x" className="close-img mr-2 mt-3" onClick={e => closeNav()}/></div>
       </div>
       <div className="nav-body mt-4">
         <div className="d-flex mb-4 ml-4 align-items-center">
@@ -14,7 +14,7 @@ const Navigation = ({closeNav}) => {
           <Link to={'/myProfile'}><h5 className="text-white">My Profile</h5></Link>
         </div>
         <div className="d-flex mb-4 ml-4 align-items-center">
-          <img className="nav-img mb-2 mr-4" src="./img/branding/my-schedule-white.svg" />
+          <img className="nav-img mb-2 mr-4" src="./img/branding/my-schedule-white.svg" alt="schedule" />
           <Link to={'/'}><h5 className="text-white">My Schedules</h5></Link>
         </div>
         <div className="d-flex mb-4 ml-4 align-items-center">
