962b0d19499fc7fb84bba0ec9f393d228eae7836
Stanley <snicholl@gmail.com>
2018-01-05 10:55:43 -0800

    item time fix, schedule dash load fix

--- src/components/ScheduleTile.js
+++ src/components/ScheduleTile.js
@@ -3,16 +3,16 @@ import { Link } from 'react-router-dom'
 
 const ScheduleTile = ({planId, name, active, startTime}) => {
   return (
-    
+
   <div className="schedule-card mb-3 col-5">
     <div className="d-flex flex-column schedule-card-content">
       <Link to={`/inProgressSchedule/${planId}`} >
         <h5 className="mt-3 font-weight-bold text-white">{name}</h5>
-        <p>{startTime}</p>
+        <p className="start-time">{startTime}</p>
       </Link>
     </div>
   </div>
-  
+
   );
 }
 
